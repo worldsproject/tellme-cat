b12f161de7da88c24f4e61cfe9ed3f240b202aae
Tim Butram <atrus6@gmail.com>
2013-04-03 20:52:00 -0400

    Added login requirements to some pages

--- followup/urls.py
+++ followup/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -10,9 +11,9 @@ urlpatterns = patterns('followup.views',
     # url(r'^$', 'followup.views.home', name='home'),
     # url(r'^followup/', include('followup.foo.urls')),
     url(r'^add_url', 'add'),
-    url(r'^add', TemplateView.as_view(template_name='add_url.html')),
+    url(r'^add', login_required(TemplateView.as_view(template_name='add_url.html'))),
     url(r'^respond_url', 'respond'),
-    url(r'^respond', TemplateView.as_view(template_name='respond.html')),
+    url(r'^respond', login_required(TemplateView.as_view(template_name='respond.html'))),
     url(r'^register_user', 'register_user'),
     url(r'^register', TemplateView.as_view(template_name='register.html')),
     url(r'^list', 'list_page'),
