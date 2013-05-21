6d4bef9a9ed7faf39d9ad59cf278fd8d414afddb
Tim Butram <atrus6@gmail.com>
2013-05-20 23:11:42 -0400

    Refixed settings

--- followup/settings.py
+++ followup/settings.py
@@ -87,12 +87,13 @@ TEMPLATE_LOADERS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.contrib.auth.context_processors.auth',
-    'django.contrib.messages.context_processors.messages',
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
     'django_browserid.context_processors.browserid',
     )
 
@@ -173,4 +174,5 @@ LOGGING = {
 }
 
 LOGIN_URL = '/login'
-SITE_URL = '*'
+LOGIN_REDIRECT_URL = '/list'
+SITE_URL = 'http://localhost:8000'
