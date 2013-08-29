65c5f1c34b13e54ef78aea8a92161028b6abea10
Tim Butram <tbutram@worldsproject.org>
2013-08-29 15:25:56 -0400

    Feeds display newest followups

--- followup/feeds.py
+++ followup/feeds.py
@@ -1,12 +1,16 @@
 from django.contrib.syndication.views import Feed
+from models import FollowUp, Story
 
 class FollowUps(Feed):
     title = "Latest Follow Ups"
     link = "/followups"
     description = "An up-to-date list of Follow Ups to the stories you're interested in."
     
-    def items(self):
-        return self.request.story_set.all().followup_set.all()[0:10]
+    def get_object(self, request):
+        return FollowUp.objects.filter(story__in = request.user.story_set.all())
+
+    def items(self, obj):
+        return obj
 
     def item_title(self, item):
         return "Follow up to the story: " + item.story.title
